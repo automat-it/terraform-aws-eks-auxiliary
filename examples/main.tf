5e330c1c6f528b47cd259c4dcedaf34514f0597b
Andrii Pidiablonskyi <andrii.pidiablonskyi@automat-it.com>
2024-04-16 12:20:39 +0300

    testing

--- examples/main.tf
+++ examples/main.tf
@@ -10,18 +10,6 @@ terraform {
       version = "~> 5.40"
       source  = "hashicorp/aws"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.20"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.9.0"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
   }
 }
 
