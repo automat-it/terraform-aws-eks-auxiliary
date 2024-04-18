a5d8116ecca17f43075aa1133e6210b3cf1f0242
Andrii Pidiablonskyi <andrii.pidiablonskyi@automat-it.com>
2024-04-18 06:08:00 +0300

    terraform_ci

--- examples/main.tf
+++ examples/main.tf
@@ -39,6 +39,18 @@ provider "aws" {
   alias  = "virginia"
 }
 
+provider "kubernetes" {
+  host                   = local.cluster_endpoint
+  cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", local.cluster_name, "--role-arn", local.deploy_role]
+  }
+}
+
 provider "helm" {
   kubernetes {
     host                   = local.cluster_endpoint
