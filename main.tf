e297735210eb296227e799e40ea120117739f579
alexanderkarlashov <60932164+alexanderkarlashov@users.noreply.github.com>
2024-04-18 22:46:23 +0300

    Remove EKS admin role

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 # Please note aws-auth CM was updated by EKS TF module. Now just need to create it on IAM.
-resource "aws_iam_role" "eks-admin" {
-  name = "${var.basename}-EKS-Admin-ROLE"
+#resource "aws_iam_role" "eks-admin" {
+#  name = "${var.basename}-EKS-Admin-ROLE"
 
-  # Allow EC2 instances and users to assume the role
-  assume_role_policy = <<-POLICY
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Effect": "Allow",
-          "Action": "sts:AssumeRole",
-          "Principal": {"Service": "ec2.amazonaws.com"}
-        },
-        {
-          "Effect": "Allow",
-          "Action": "sts:AssumeRole",
-          "Principal": {"AWS": "arn:aws:iam::${var.aws_account}:root"}
-        }
-      ]
-    }
-    POLICY
-}
+#  # Allow EC2 instances and users to assume the role
+#  assume_role_policy = <<-POLICY
+#    {
+#      "Version": "2012-10-17",
+#      "Statement": [
+#        {
+#          "Effect": "Allow",
+#          "Action": "sts:AssumeRole",
+#          "Principal": {"Service": "ec2.amazonaws.com"}
+#        },
+#        {
+#          "Effect": "Allow",
+#          "Action": "sts:AssumeRole",
+#          "Principal": {"AWS": "arn:aws:iam::${var.aws_account}:root"}
+#        }
+#      ]
+#    }
+#    POLICY
+#}
 
 locals {
   # Helm versions
