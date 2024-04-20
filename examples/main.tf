81ab6e1d8a1d0822ddec6a7cd63fbaf0a744478a
Andrii Pidiablonskyi <andrii.pidiablonskyi@automat-it.com>
2024-04-20 08:05:42 +0300

    terraform_ci

--- examples/main.tf
+++ examples/main.tf
@@ -38,7 +38,6 @@ provider "aws" {
 provider "kubernetes" {
   host                   = local.cluster_endpoint
   cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-  config_path            = "/root/.kube/config"
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
@@ -52,7 +51,6 @@ provider "helm" {
   kubernetes {
     host                   = local.cluster_endpoint
     cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-    config_path            = "/root/.kube/config"
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
