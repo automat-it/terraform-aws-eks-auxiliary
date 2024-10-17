695bcaa4898ad0f3b7f293c5c9e41b1b696ed6ba
Mykhailo Lavrentiev <mykhailo.lavrentiev@automat-it.com>
2024-10-17 17:11:22 +0300

    fix: example typo

--- examples/eks-auxiliary.tf
+++ examples/eks-auxiliary.tf
@@ -22,8 +22,8 @@ module "eks-aux" {
       enabled = false
     }
     karpenter = {
-      enabled                          = true
-      karpenter_node_security_group_id = module.eks.node_security_group_id
+      enabled                = true
+      node_security_group_id = module.eks.node_security_group_id
     }
     external-dns = {
       enabled = true
