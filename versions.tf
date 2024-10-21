d99eece0f100478ed1200fc311d359cbe844a153
Yurii Liaskovets <yurii.liaskovets@gmail.com>
2024-10-21 16:38:34 +0300

    Update terraform required version and providers

--- versions.tf
+++ versions.tf
@@ -1,19 +1,10 @@
 terraform {
-  required_version = ">= 1.8.4"
+  required_version = "~> 1.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.20"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.9.0"
-    }
+    aws        = ">= 5.0"
+    kubernetes = ">= 2.20"
+    helm       = ">= 2.9.0"
     kubectl = {
       source  = "alekc/kubectl"
       version = ">= 2.0"
