f47c5b052a01ac0eae3029067e37d1b8132e10a4
Maksym Butusov <maksym.butusov@automat-it.com>
2025-01-30 16:06:20 +0200

    fixed condition for budgets

--- variables.tf
+++ variables.tf
@@ -138,13 +138,13 @@ variable "services" {
       default_nodeclass_instance_cpu      = optional(list(string), ["2", "4"])
       deploy_default_nodepool             = optional(bool, true)
       default_nodepool_cpu_limit          = optional(string, "100")
-      enable_budgets                      = false
-      budgets = [
+      enable_budgets                      = otional(bool, false)
+      budgets = optiona(any, [
         { nodes = "10%" },
         { nodes = "3" },
         { nodes = "0", schedule = "0 9 * * sat-sun", duration = "24h" },
         { nodes = "0", schedule = "0 17 * * mon-fri", duration = "16h", reasons = ["Drifted"] }
-      ]
+      ])
       default_nodepool_capacity_type    = optional(list(string), ["on-demand"])
       default_nodepool_yaml             = optional(string)
       default_nodeclass_yaml            = optional(string)
