2494dbe68ce533969649e687c266b2904db22306
Rodion Serhieiev <rodionmdk@gmail.com>
2025-02-10 11:13:48 +0200

    fix(argocd): fix namespace reference, move namespace creation, fix hardcode in namespace

--- main.tf
+++ main.tf
@@ -14,14 +14,6 @@ resource "kubernetes_namespace_v1" "security" {
   }
 }
 
-# argocd
-resource "kubernetes_namespace_v1" "argocd" {
-  count = var.services.argocd.enabled ? 1 : 0
-  metadata {
-    name = "argocd"
-  }
-}
-
 ### EKS data
 data "aws_eks_cluster" "this" {
   name = var.cluster_name
