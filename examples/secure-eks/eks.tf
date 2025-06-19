9b81cc25dc3652498bb71409ebeaec77b39d174b
bohdan.sariohlo <bohdan.sariohlo@automat-it.com>
2025-06-19 15:34:52 +0200

    - Changed minimium helm version to 3.0.0 - Adjusted helm provider syntax in order to meet new requirements

--- examples/secure-eks/eks.tf
+++ examples/secure-eks/eks.tf
@@ -31,16 +31,18 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.this.token
   }
-  registry {
-    url      = "oci://public.ecr.aws"
-    username = data.aws_ecrpublic_authorization_token.token.user_name
-    password = data.aws_ecrpublic_authorization_token.token.password
-  }
+  registries = [ 
+    {
+      url      = "oci://public.ecr.aws"
+      username = data.aws_ecrpublic_authorization_token.token.user_name
+      password = data.aws_ecrpublic_authorization_token.token.password
+    }
+  ]
 }
 
 provider "kubectl" {
