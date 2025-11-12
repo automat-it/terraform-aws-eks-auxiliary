7908c31ee40183a60a1568f6eb29fe0cac855c37
Mykola Borchuk <mykola.borchuk@automat-it.com>
2025-11-12 13:25:48 +0100

    consolidation policy

--- karpenter.tf
+++ karpenter.tf
@@ -147,7 +147,9 @@ locals {
       limits:
         cpu: ${var.services.karpenter.default_nodepool_cpu_limit}
       disruption:
-        consolidationPolicy: WhenEmpty
+  %{if coalesce(var.services.karpenter.consolidation_policy, "default") != "default"~}
+        consolidationPolicy: ${var.services.karpenter.consolidation_policy}
+  %{endif}
         consolidateAfter: 30s
   %{if var.services.karpenter.enable_budgets}
         budgets: ${jsonencode(var.services.karpenter.budgets)}
