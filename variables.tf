735c150c159053af91fcaeb45f682bbd04910b24
Inception <62402529+maksym-butusov@users.noreply.github.com>
2025-11-21 15:45:21 +0200

    feature/additional-node-pool (#80)
    * feature/additional-node-pool
    
    * feature/additional-node-pool
    
    ---------
    
    Co-authored-by: Maksym Butusov <maksym.butusov@automat-it.com>
    # Conflicts:
    #	README.md
    #	variables.tf

--- variables.tf
+++ variables.tf
@@ -206,6 +206,7 @@ variable "services" {
       default_nodepool_instance_cpu_manufacturer = optional(list(string), [])
       default_nodepool_cpu_limit                 = optional(string, "100")
       consolidation_policy                       = optional(string)
+      additional_nodepools_yaml                  = optional(map(any), {})
       enable_budgets                             = optional(bool, false)
       budgets = optional(any, [
         { nodes = "10%" },
