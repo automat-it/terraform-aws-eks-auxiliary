2408d9b009604704c55e755bb7f984dbc92dcaa1
Inception <62402529+maksym-butusov@users.noreply.github.com>
2026-03-06 17:15:32 +0200

    feature/bump-helm-versions (#89)
    Co-authored-by: Maksym Butusov <maksym.butusov@automat-it.com>

--- variables.tf
+++ variables.tf
@@ -72,7 +72,7 @@ variable "services" {
     argocd = optional(object({
       enabled              = bool
       chart_name           = optional(string, "argocd")
-      helm_version         = optional(string, "9.0.5")
+      helm_version         = optional(string, "9.4.2")
       namespace            = optional(string, "argocd")
       service_account_name = optional(string, "argocd-sa")
       node_selector        = optional(map(string), { pool = "system" })
@@ -98,7 +98,7 @@ variable "services" {
     aws-alb-ingress-controller = optional(object({
       enabled              = bool
       chart_name           = optional(string, "aws-alb-ingress-controller")
-      helm_version         = optional(string, "1.14.1")
+      helm_version         = optional(string, "1.17.1")
       namespace            = optional(string, "general")
       service_account_name = optional(string, "aws-alb-ingress-controller-sa")
       default_ssl_policy   = optional(string, "ELBSecurityPolicy-TLS13-1-2-2021-06")
@@ -118,7 +118,7 @@ variable "services" {
     cluster-autoscaler = optional(object({
       enabled              = bool
       chart_name           = optional(string, "cluster-autoscaler")
-      helm_version         = optional(string, "9.52.1")
+      helm_version         = optional(string, "9.55.0")
       namespace            = optional(string, "general")
       service_account_name = optional(string, "autoscaler-sa")
       node_selector        = optional(map(string), { pool = "system" })
@@ -137,7 +137,7 @@ variable "services" {
     external-dns = optional(object({
       enabled              = bool
       chart_name           = optional(string, "external-dns")
-      helm_version         = optional(string, "1.19.0")
+      helm_version         = optional(string, "1.20.0")
       namespace            = optional(string, "general")
       service_account_name = optional(string, "external-dns-sa")
       node_selector        = optional(map(string), { pool = "system" })
@@ -157,7 +157,7 @@ variable "services" {
     external-secrets = optional(object({
       chart_name           = optional(string, "external-secrets")
       enabled              = bool
-      helm_version         = optional(string, "0.20.4")
+      helm_version         = optional(string, "2.0.0")
       namespace            = optional(string, "general")
       service_account_name = optional(string, "external-secrets-sa")
       node_selector        = optional(map(string), { pool = "system" })
@@ -178,7 +178,7 @@ variable "services" {
       chart_name           = optional(string, "karpenter")
       chart_crd_name       = optional(string, "karpenter-crd")
       enabled              = bool
-      helm_version         = optional(string, "1.8.2")
+      helm_version         = optional(string, "1.9.0")
       manage_crd           = optional(bool, false) # Whether to directly manage CRD by Terraform. If false, CRD will be installed by the karpenter helm by dependency. If true, CRD will be installed with additional helm via terraform. Reference: https://github.com/aws/karpenter-provider-aws/tree/main/charts/karpenter-crd
       namespace            = optional(string, "general")
       service_account_name = optional(string, "karpenter")
@@ -231,7 +231,7 @@ variable "services" {
     keda = optional(object({
       chart_name           = optional(string, "keda")
       enabled              = bool
-      helm_version         = optional(string, "2.18.1")
+      helm_version         = optional(string, "2.19.0")
       namespace            = optional(string, "general")
       service_account_name = optional(string, "keda-sa")
       node_selector        = optional(map(string), { pool = "system" })
